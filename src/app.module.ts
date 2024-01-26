3d31039c77da1037898d25e8ea5626b1d3aaea01
Max Almonte <maxalmonte14@hotmail.com>
2024-01-26 18:54:18 -0400

    Add driver module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
-  imports: [],
+  imports: [DriverModule],
   controllers: [AppController],
   providers: [AppService],
 })
