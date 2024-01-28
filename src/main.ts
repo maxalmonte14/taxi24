003a673ba8b663e046b129dbacd1e7055e168cdb
Max Almonte <maxalmonte14@hotmail.com>
2024-01-28 13:57:24 -0400

    Add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Taxi24 API')
+    .setDescription('The Taxi24 API specification')
+    .setVersion('1.0')
+    .addTag('drivers')
+    .addTag('passengers')
+    .addTag('rides')
+    .build();
 
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, options));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
