50b6553f307a869fe1b7c3f609b6d1cb2971a9fe
Max Almonte <maxalmonte14@hotmail.com>
2024-01-29 12:20:39 -0400

    Remove Swagger plugin

--- src/ride/entities/ride.entity.ts
+++ src/ride/entities/ride.entity.ts
@@ -1,58 +1,65 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 import { IsInt, Min } from 'class-validator';
 
 export class Ride {
-  /**
-   * The identifier of the resource
-   */
   @IsInt()
   @Min(1)
+  @ApiProperty({ description: 'The identifier of the resource' })
   id: number;
 
-  /**
-   * A valid latitude represented as a floating point number
-   * @example 18.4636960171801
-   */
   @Expose({ name: 'originLatitude' })
+  @ApiProperty({
+    description: 'A valid latitude represented as a floating point number',
+    example: '18.4636960171801',
+    name: 'originLatitude',
+  })
   origin_latitude: string;
 
-  /**
-   * A valid longitude represented as a floating point number
-   * @example -69.93474882920843
-   */
   @Expose({ name: 'originLongitude' })
+  @ApiProperty({
+    description: 'A valid longitude represented as a floating point number',
+    example: '-69.93474882920843',
+    name: 'originLongitude',
+  })
   origin_longitude: string;
 
-  /**
-   * A valid latitude represented as a floating point number
-   * @example 18.47553627458603
-   */
   @Expose({ name: 'destinationLatitude' })
+  @ApiProperty({
+    description: 'A valid latitude represented as a floating point number',
+    example: '18.47553627458603',
+    name: 'destinationLatitude',
+  })
   destination_latitude: string;
 
-  /**
-   * A valid longitude represented as a floating point number
-   * @example -69.94349904538785
-   */
   @Expose({ name: 'destinationLongitude' })
+  @ApiProperty({
+    description: 'A valid longitude represented as a floating point number',
+    example: '-69.94349904538785',
+    name: 'destinationLongitude',
+  })
   destination_longitude: string;
 
-  /**
-   * A boolean representing if the ride has been completed
-   */
   @Expose({ name: 'isCompleted' })
+  @ApiProperty({
+    description: 'A boolean representing if the ride has been completed',
+    name: 'isCompleted',
+  })
   is_completed: boolean;
 
-  /**
-   * The identifier of the driver associated with this resource
-   */
   @Expose({ name: 'driverId' })
+  @ApiProperty({
+    description: ' The identifier of the driver associated with this resource',
+    name: 'driverId',
+  })
   driver_id: number;
 
-  /**
-   * The identifier of the passenger associated with this resource
-   */
   @Expose({ name: 'passengerId' })
+  @ApiProperty({
+    description:
+      ' The identifier of the passenger associated with this resource',
+    name: 'passengerId',
+  })
   passenger_id: number;
 
   constructor(partial: Partial<Ride>) {
